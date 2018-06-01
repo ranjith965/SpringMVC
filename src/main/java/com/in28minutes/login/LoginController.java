eb52fde9254380b1934934fc5a8b9b8a4aaf97af
ranjith965 <ranjith.kumar@juspay.in>
2018-06-01 15:16:48 +0530

    removed login servlet

--- src/main/java/com/in28minutes/login/LoginController.java
+++ src/main/java/com/in28minutes/login/LoginController.java
@@ -1,6 +1,7 @@
-package com.in28minutes.springmvc;
+package com.in28minutes.login;
 
 import org.springframework.stereotype.Controller;
+import com.in28minutes.jee.*;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LoginController {
 	
+	LoginService service = new LoginService();
+	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
 		return "/login";
@@ -18,6 +21,11 @@ public class LoginController {
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String handleLoginRequest(@RequestParam String name, @RequestParam String password,
 			                         ModelMap model ) {
+		
+		if(!service.validateUser(name,password)) {
+			model.put("errorMessage", "Invalid Credentials");
+			return "/login";
+		}
 		model.put("name", name);
 		model.put("password", password);
 		return "/welcome";
