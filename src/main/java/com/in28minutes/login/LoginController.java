92fd8a73bdb097b942a4b3708cc4898bc820028e
ranjith965 <ranjith.kumar@juspay.in>
2018-06-01 15:30:29 +0530

    using Autowiring and Dependency injection with @Service and @Autowired

--- src/main/java/com/in28minutes/login/LoginController.java
+++ src/main/java/com/in28minutes/login/LoginController.java
@@ -1,7 +1,7 @@
 package com.in28minutes.login;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import com.in28minutes.jee.*;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,7 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class LoginController {
 	
-	LoginService service = new LoginService();
+	@Autowired
+	LoginService service;
 	
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String showLoginPage() {
