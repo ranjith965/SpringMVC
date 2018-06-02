61d8e6541025fdbb81067ddac055397a2f02705e
ranjith965 <ranjith.kumar@juspay.in>
2018-06-02 17:58:34 +0530

    added addToto api

--- src/main/java/com/in28minutes/todo/TodoController.java
+++ src/main/java/com/in28minutes/todo/TodoController.java
@@ -1,10 +1,13 @@
 package com.in28minutes.todo;
 
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TodoController {
@@ -13,8 +16,20 @@ public class TodoController {
 	private TodoService service;
 
 	@RequestMapping(value = "/list-todos", method = RequestMethod.GET)
-	public String showLoginPage(ModelMap model) {
+	public String listTodos(ModelMap model) {
 		model.addAttribute("todos", service.retrieveTodos("in28Minutes"));
 		return "list-todos";
+	}
+	
+	@RequestMapping(value = "/add-todo", method = RequestMethod.GET)
+	public String showAddTodoPage() {
+		return "add-todo";
+	}
+	
+	@RequestMapping(value = "/add-todo", method = RequestMethod.POST)
+	public String addTodo(@RequestParam String desc,ModelMap model) {
+		service.addTodo("in28Minutes", desc, new Date(), false);
+		model.clear();
+		return "redirect:list-todos";
 	}
 }
--- src/main/webapp/WEB-INF/views/list-todos.jsp
+++ src/main/webapp/WEB-INF/views/list-todos.jsp
@@ -6,5 +6,7 @@
 <H1>Hi ${name}</H1>
 <H1>Your todos</H1>
  ${todos}
+ </br>
+ <a href="/add-todo">Add Todo</a>
 </body>
 </html>
